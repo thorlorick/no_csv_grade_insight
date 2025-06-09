55be6c0893e7e4cb4e1bca749712893560b556d5
ThorloRick <thorlorick@gmail.com>
2025-06-09 09:56:27 -0400

    Update main.py
    added app.include_router(auth.router)

--- app/main.py
+++ app/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from app.routers import users, grades, courses
+from app import auth
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(grades.router)
 app.include_router(courses.router)
+app.include_router(auth.router)
 
 @app.get("/health")
 def health_check():
